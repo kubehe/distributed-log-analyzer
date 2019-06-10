b43836b64b52f63ca7ba5ba04318ed9aa9c1cffa
kubehe <jakub.k.b@hotmail.com>
2019-06-11 00:00:43 +0200

    added slave

--- slave/src/main/java/pl/kubehe/slave/SlaveApplication.java
+++ slave/src/main/java/pl/kubehe/slave/SlaveApplication.java
@@ -1,21 +1,17 @@
-package pl.kubehe.master;
+package pl.kubehe.slave;
 
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Delivery;
-import io.vavr.control.Try;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.codec.multipart.FilePart;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -24,16 +20,15 @@ import reactor.rabbitmq.*;
 @Slf4j
 @RestController
 @SpringBootApplication
-public class MasterApplication {
+public class SlaveApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(MasterApplication.class, args);
+    SpringApplication.run(SlaveApplication.class, args);
   }
 }
 
 @Configuration
 class QueueConfiguration {
-
   static final String JOB_QUEUE = "distributed.log.analyzer.job";
   static final String RESULT_QUEUE = "distributed.log.analyzer.result";
 
@@ -63,34 +58,22 @@ class QueueConfiguration {
   }
   @Bean
   Flux<Delivery> deliveryFluxBean(Receiver receiver) {
-    return receiver.consumeNoAck(RESULT_QUEUE);
+    return receiver.consumeNoAck(JOB_QUEUE);
   }
 }
 
+@Component
 @AllArgsConstructor
 @Slf4j
-@RestController
-class ApiController {
+class QueueService {
 
   private Sender sender;
 
-  private Flux<Delivery> deliveryFlux;
+  @RabbitListener(queues = QueueConfiguration.JOB_QUEUE)
+  public Mono<Void> handleJobs(byte[] message) {
 
-  @PostMapping(value = "/upload")
-  public Mono<Void> upload(@RequestPart("file") Mono<FilePart> file) {
-    return sender.send(
-      file.flatMapMany(filePart -> {
-        log.info("processing file: " + filePart.filename());
-        return filePart.content().map(dataBuffer ->
-          Try.of(() -> dataBuffer.asInputStream().readAllBytes()).getOrElse("{{ERROR}}".getBytes())
-        );
-      }).map(bytes -> new OutboundMessage("", QueueConfiguration.JOB_QUEUE, bytes)))
-      .doOnSuccess(v -> log.info("finished processing file")).doOnError(throwable -> log.error(throwable.getMessage()));
+    return sender.send(Mono.just(message).map(msg -> new OutboundMessage("", QueueConfiguration.RESULT_QUEUE, (new String(msg) + " processed").getBytes())));
   }
 
-  @GetMapping(value = "/result", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public Flux<String> getResult() {
-    return deliveryFlux.map(delivery -> new String(delivery.getBody()));
-  }
 
 }
