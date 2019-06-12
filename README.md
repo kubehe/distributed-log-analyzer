043b20aa91e40f119297d2ea6dd16aa979a545d8
kubehe <jakub.k.b@hotmail.com>
2019-06-12 11:03:13 +0200

    front cd and started docs

---
+++ README.md
@@ -0,0 +1,13 @@
+# Projekt na zajęcia PRORR - Jakub Bączek, Szymon Petruczynik
+
+## Temat
+
+## Założenia
+
+## Architektura 
+
+## Technologie
+
+## Instrukcja obsługi
+
+## Wnioski
--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,14 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import Rx from 'rx-dom';
 
-import {LineChart, Line, CartesianGrid, XAxis, YAxis} from 'recharts';
+import {LineChart, Line, CartesianGrid, XAxis, YAxis, Legend} from 'recharts';
 
-const RenderLineChart = ({data}) => (
+const MethodLineChart = ({data}) => (
   <LineChart width={600} height={300} data={data}>
     <Line type="monotone" dataKey="DELETE" stroke="#DE1738"/>
     <Line type="monotone" dataKey="POST" stroke="#00416A"/>
     <Line type="monotone" dataKey="GET" stroke="#51FF0D"/>
     <Line type="monotone" dataKey="PUT" stroke="#8884d8"/>
+    <Legend/>
     <CartesianGrid stroke="#ccc"/>
     <XAxis dataKey="id"/>
     <YAxis/>
@@ -25,8 +26,7 @@ const App = () => {
     source.subscribe((e) => {
       const data = JSON.parse(e);
       setState(prev => [...prev.slice(0, prev.length > 100 ? 100 : prev.length), {
-        ...data.method,
-        id: data.protocol["HTTP/1.0"]
+        ...data,
       }]);
     })
 
@@ -34,7 +34,9 @@ const App = () => {
   if (state == null || state.length < 1) return null;
   return (
     <div>
-      <RenderLineChart data={state}/>
+      <MethodLineChart data={state.map((data) => ({...data.method, id: data.protocol["HTTP/1.0"]}))}/>
+
+
 
     </div>
   );
