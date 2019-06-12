ec9bcd579582f635e22de9afd305c22019cd7bae
kubehe <jakub.k.b@hotmail.com>
2019-06-12 06:47:39 +0200

    need to change buffer to memoize last element in flux so that i can concatenate it with the incoming value

--- master/src/main/java/pl/kubehe/master/MasterApplication.java
+++ master/src/main/java/pl/kubehe/master/MasterApplication.java
@@ -1,9 +1,15 @@
 package pl.kubehe.master;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Delivery;
+import io.vavr.Function1;
+import io.vavr.Function2;
 import io.vavr.control.Try;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -21,6 +27,11 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.rabbitmq.*;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.Optional.ofNullable;
+
 @Slf4j
 @RestController
 @SpringBootApplication
@@ -34,17 +45,17 @@ public class MasterApplication {
 @Configuration
 class QueueConfiguration {
 
-  static final String JOB_QUEUE = "distributed.log.analyzer.job";
-  static final String RESULT_QUEUE = "distributed.log.analyzer.result";
+  static final String PROCESSING_QUEUE = "distributed.log.analyzer.processing";
+  static final String AGGREGATION_QUEUE = "distributed.log.analyzer.aggregation";
 
   @Bean
   Queue jobQueue() {
-    return new Queue(JOB_QUEUE);
+    return new Queue(PROCESSING_QUEUE);
   }
 
   @Bean
   Queue resultQueue() {
-    return new Queue(RESULT_QUEUE);
+    return new Queue(AGGREGATION_QUEUE);
   }
 
   @Bean
@@ -63,7 +74,7 @@ class QueueConfiguration {
   }
   @Bean
   Flux<Delivery> deliveryFluxBean(Receiver receiver) {
-    return receiver.consumeNoAck(RESULT_QUEUE);
+    return receiver.consumeNoAck(AGGREGATION_QUEUE);
   }
 }
 
@@ -74,6 +85,8 @@ class ApiController {
 
   private Sender sender;
 
+  private ObjectMapper objectMapper;
+
   private Flux<Delivery> deliveryFlux;
 
   @PostMapping(value = "/upload")
@@ -82,15 +95,65 @@ class ApiController {
       file.flatMapMany(filePart -> {
         log.info("processing file: " + filePart.filename());
         return filePart.content().map(dataBuffer ->
-          Try.of(() -> dataBuffer.asInputStream().readAllBytes()).getOrElse("{{ERROR}}".getBytes())
+          Try.of(() -> dataBuffer.asInputStream().readAllBytes()).getOrElse("".getBytes())
         );
-      }).map(bytes -> new OutboundMessage("", QueueConfiguration.JOB_QUEUE, bytes)))
+      }).map(bytes -> new OutboundMessage("", QueueConfiguration.PROCESSING_QUEUE, bytes)))
       .doOnSuccess(v -> log.info("finished processing file")).doOnError(throwable -> log.error(throwable.getMessage()));
   }
 
   @GetMapping(value = "/result", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public Flux<String> getResult() {
-    return deliveryFlux.map(delivery -> new String(delivery.getBody()));
+  public Flux<AggregatedLogs> getResult() {
+    return deliveryFlux.map(delivery -> new String(delivery.getBody()))
+
+      .map(msg -> Try.of(() -> objectMapper.readValue(msg, AggregatedLogs.class)).get())
+      .buffer(2, 1)
+      .map(list -> Mapper.joinAggregatedLogs.apply(list.get(0), list.get(1)));
   }
 
 }
+
+class Mapper {
+  static Function2<AggregatedLogs, AggregatedLogs, AggregatedLogs> joinAggregatedLogs = (a1, a2) -> {
+    if (a1 == null && a2 == null) throw new IllegalArgumentException("at least first arg cannot be null!");
+    if (a2 == null) return a1;
+    Function1<Function1<AggregatedLogs, Map<String, Long>>, Map<String, Long>> joinField = (mapper) -> {
+      var joinedMap = mapper.apply(a1).entrySet().stream()
+        .map(entry -> {
+          var value = ofNullable(mapper.apply(a2).get(entry.getKey())).map(size2 -> size2 + entry.getValue()).orElse(entry.getValue());
+          return Map.entry(entry.getKey(), value);
+        }).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+      mapper.apply(a2).forEach(joinedMap::putIfAbsent);
+      return joinedMap;
+    };
+    return AggregatedLogs.builder()
+      .ip(joinField.apply(AggregatedLogs::getIp))
+      .date(joinField.apply(AggregatedLogs::getDate))
+      .method(joinField.apply(AggregatedLogs::getMethod))
+      .endpoint(joinField.apply(AggregatedLogs::getEndpoint))
+      .protocol(joinField.apply(AggregatedLogs::getProtocol))
+      .status(joinField.apply(AggregatedLogs::getStatus))
+      .port(joinField.apply(AggregatedLogs::getPort))
+      .origin(joinField.apply(AggregatedLogs::getOrigin))
+      .userAgent(joinField.apply(AggregatedLogs::getUserAgent))
+      .build();
+  };
+}
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class AggregatedLogs {
+  private Map<String, Long> ip;
+  private Map<String, Long> date;
+  private Map<String, Long> method;
+  private Map<String, Long> endpoint;
+  private Map<String, Long> protocol;
+  private Map<String, Long> status;
+  private Map<String, Long> port;
+  private Map<String, Long> origin;
+  private Map<String, Long> userAgent;
+}
+
+
--- slave/src/main/java/pl/kubehe/slave/SlaveApplication.java
+++ slave/src/main/java/pl/kubehe/slave/SlaveApplication.java
@@ -1,8 +1,12 @@
 package pl.kubehe.slave;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Delivery;
-import lombok.AllArgsConstructor;
+import io.vavr.Function1;
+import io.vavr.control.Try;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -17,6 +21,16 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.rabbitmq.*;
 
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
+
+
 @Slf4j
 @RestController
 @SpringBootApplication
@@ -29,19 +43,24 @@ public class SlaveApplication {
 
 @Configuration
 class QueueConfiguration {
-  static final String JOB_QUEUE = "distributed.log.analyzer.job";
-  static final String RESULT_QUEUE = "distributed.log.analyzer.result";
+  static final String PROCESSING_QUEUE = "distributed.log.analyzer.processing";
+  static final String CALCULATION_QUEUE = "distributed.log.analyzer.calculation";
+  static final String AGGREGATION_QUEUE = "distributed.log.analyzer.aggregation";
 
   @Bean
   Queue jobQueue() {
-    return new Queue(JOB_QUEUE);
+    return new Queue(PROCESSING_QUEUE);
   }
 
   @Bean
-  Queue resultQueue() {
-    return new Queue(RESULT_QUEUE);
+  Queue calculationQueue() {
+    return new Queue(CALCULATION_QUEUE);
   }
 
+  @Bean
+  Queue aggregationQueue() {
+    return new Queue(CALCULATION_QUEUE);
+  }
   @Bean
   Mono<Connection> connectionMono(ConnectionFactory connectionFactory) {
     return Mono.fromCallable(() -> connectionFactory.createConnection().getDelegate()).cache();
@@ -56,24 +75,135 @@ class QueueConfiguration {
   Receiver receiver(Mono<Connection> connectionMono) {
     return RabbitFlux.createReceiver(new ReceiverOptions().connectionMono(connectionMono));
   }
-  @Bean
-  Flux<Delivery> deliveryFluxBean(Receiver receiver) {
-    return receiver.consumeNoAck(JOB_QUEUE);
-  }
+//  @Bean
+//  Flux<AcknowledgableDelivery> deliveryFluxBean(Receiver receiver) {
+//    return receiver.consumeManualAck(PROCESSING_QUEUE);
+//  }
 }
 
 @Component
 @AllArgsConstructor
 @Slf4j
-class QueueService {
+class CalculationService {
+
+  private final String regex = "^(\\S+) (\\S+) (\\S+) \\[([\\w:/]+\\s[+\\-]\\d{4})\\] \"(\\S+)\\s?(\\S+)?\\s?(\\S+)?\" (\\d{3}|-) (\\d+|-)\\s?\"?([^\"]*)\"?\\s?\"?([^\"]*)?\"?$";
 
   private Sender sender;
 
-  @RabbitListener(queues = QueueConfiguration.JOB_QUEUE)
+  private ObjectMapper objectMapper;
+
+  @RabbitListener(queues = QueueConfiguration.PROCESSING_QUEUE)
   public Mono<Void> handleJobs(byte[] message) {
 
-    return sender.send(Mono.just(message).map(msg -> new OutboundMessage("", QueueConfiguration.RESULT_QUEUE, (new String(msg) + " processed").getBytes())));
+    var pattern = Pattern.compile(regex);
+
+
+    return sender.send(
+      Mono.just(message)
+        .map(String::new)
+        .map(msg -> msg.split("\n"))
+        .flatMapMany(Flux::fromArray)
+        .map(pattern::matcher)
+        .filter(Matcher::find)
+        .map(LogsMapper::logsMapper)
+        .collectList()
+        .map(msg -> Try.of(() -> objectMapper.writeValueAsBytes(msg)).get())
+        .map(msg -> new OutboundMessage("", QueueConfiguration.CALCULATION_QUEUE, msg)));
   }
 
+}
 
+@Component
+@AllArgsConstructor
+class AggregationService {
+
+  private Sender sender;
+
+  private ObjectMapper objectMapper;
+
+  @RabbitListener(queues = QueueConfiguration.CALCULATION_QUEUE)
+  public Mono<Void> handleJobs(byte[] message) {
+
+    return sender.send(
+      Mono.just(message)
+        .map(msg -> Try.of(() -> (List<Logs>) objectMapper.readValue(msg, new TypeReference<List<Logs>>() {
+        })).get())
+        .map(LogsMapper::mapLogs)
+        .map(msg -> Try.of(() -> objectMapper.writeValueAsBytes(msg)).get())
+        .map(msg -> new OutboundMessage("", QueueConfiguration.AGGREGATION_QUEUE, msg)));
+  }
+
+}
+
+class LogsMapper {
+
+  static Logs logsMapper(Matcher matcher) {
+    return Logs.builder()
+      .ip(matcher.group(1))
+      .date(matcher.group(4))
+      .method(matcher.group(5))
+      .endpoint(matcher.group(6))
+      .protocol(matcher.group(7))
+      .status(matcher.group(8))
+      .port(matcher.group(9))
+      .origin(matcher.group(10))
+      .userAgent(matcher.group(11))
+      .build();
+  }
+
+  static AggregatedLogs mapLogs(List<Logs> logsList) {
+
+    Function1<Function1<Logs, String>, Map<String, Long>> countByField = (mapper) -> {
+      var groupedBy = logsList.stream().collect(groupingBy(mapper));
+      return groupedBy.entrySet().stream()
+        .map(entry -> Map.entry(entry.getKey(), Long.valueOf(entry.getValue().size())))
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    };
+
+    return AggregatedLogs.builder()
+      .ip(countByField.apply(Logs::getIp))
+      .date(countByField.apply(Logs::getDate))
+      .method(countByField.apply(Logs::getMethod))
+      .endpoint(countByField.apply(Logs::getEndpoint))
+      .protocol(countByField.apply(Logs::getProtocol))
+      .status(countByField.apply(Logs::getStatus))
+      .port(countByField.apply(Logs::getPort))
+      .origin(countByField.apply(Logs::getOrigin))
+      .userAgent(countByField.apply(Logs::getUserAgent))
+      .build();
+  }
+
+}
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class AggregatedLogs {
+  private Map<String, Long> ip;
+  private Map<String, Long> date;
+  private Map<String, Long> method;
+  private Map<String, Long> endpoint;
+  private Map<String, Long> protocol;
+  private Map<String, Long> status;
+  private Map<String, Long> port;
+  private Map<String, Long> origin;
+  private Map<String, Long> userAgent;
+}
+
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class Logs {
+  private String ip;
+  private String date;
+  private String method;
+  private String endpoint;
+  private String protocol;
+  private String status;
+  private String port;
+  private String origin;
+  private String userAgent;
 }
